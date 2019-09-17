4ef93adaea1996c21ea76b8ceacc21593c28e4f0
Warren Yao <warrenyca905@gmail.com>
2019-09-17 02:23:14 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,5 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-
+  
+  get '/' do
+    erb :index
+  end
+  
+  get '/new' do
+    erb :create_puppy
+  end
+  
+  post '/puppy' do
+    @name = params[:name]
+    @breed = params[:breed]
+    @age = params[:age]
+    new_puppy = Puppy.new(@name, @breed, @age)
+    
+    erb :display_puppy
+  end
 end
